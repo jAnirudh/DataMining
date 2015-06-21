3f6d203de89d1ccf6268868ab506fdafeb8a64be
Anirudh <jonnalagadda.anirudh@gmail.com>
2015-06-21 20:32:46 +0530

    incorporated folding of training data

--- myPorterStemmer.py
+++ myPorterStemmer.py
@@ -5,9 +5,9 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 from sklearn import pipeline
 from nltk.stem.porter import PorterStemmer
-import re
 from bs4 import BeautifulSoup
 from ml_metrics import quadratic_weighted_kappa
+from sklearn.cross_validation import cross_val_predict
 
 # array declarations
 traindata = []
@@ -18,26 +18,32 @@ if __name__ == '__main__':
     train = read_csv("../train.csv").fillna("")
 
     y = train.median_relevance.values    
-    #remove html, remove non text or numeric, make query and title unique features for counts using prefix (accounted for in stopwords tweak)
+    
+    ## PORTER STEMMING ##
     stemmer = PorterStemmer()
     
     for i in range(len(y)):
         s=(" ").join(["q"+ z for z in BeautifulSoup(train["query"][i]).get_text(" ").split(" ")]) + " " + (" ").join(["z"+ z for z in BeautifulSoup(train.product_title[i]).get_text(" ").split(" ")]) + " " + BeautifulSoup(train.product_description[i]).get_text(" ")
-#        s=re.sub("[^a-zA-Z0-9]"," ", s)
         s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
         traindata.append(s)
-        #s_labels.append(str(train["median_relevance"][i]))
+
+    # Initialize tf-idf vectorization function
 
     tfv = TfidfVectorizer(min_df=3,  max_features=None,strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
  
     tfv.fit(traindata)
     X = tfv.transform(traindata)
 
-    #create sklearn pipeline, fit all, and predit test data
-    clf = pipeline.Pipeline([('svd', TruncatedSVD(n_components=400, algorithm='randomized', random_state=None, tol=0.0)),('scl', StandardScaler(copy=True, with_mean=True, with_std=True)),('svm', SVC(C=10.0, kernel='rbf', degree=3, gamma=0.0, coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, random_state=None))])
-    
+    # Initialize Model Variables #
 
-    clf.fit(X,y)
-    stemPred = clf.predict(X)
+    tSVD = TruncatedSVD(n_components=400)
+    scl  = StandardScaler()
+    svm  = SVC(C=10) 
+
+    #create sklearn pipeline
+
+    clf = pipeline.Pipeline([('tSVD', tSVD),('scl', scl),('svm', svm)])
+    
+    stemPred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
     
     print "Kappa Score for Training Data\nStemming\nScore=%f" %(quadratic_weighted_kappa(y, stemPred))
