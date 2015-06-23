bbb5fb4d2a1be9b8f188a0d8f85e01e3624fa9df
Anirudh <jonnalagadda.anirudh@gmail.com>
2015-06-23 13:06:49 +0530

    modified corpus creation in myPorterStemmer.py

--- myPorterStemmer.py
+++ myPorterStemmer.py
@@ -1,4 +1,4 @@
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
@@ -9,41 +9,63 @@ from bs4 import BeautifulSoup
 from ml_metrics import quadratic_weighted_kappa
 from sklearn.cross_validation import cross_val_predict
 
-# array declarations
-traindata = []
+#load data
+train = read_csv("../train.csv").fillna("")
+test  = read_csv("../test.csv").fillna("")
+y = train.median_relevance.values
 
-if __name__ == '__main__':
-    
-    #load data
-    train = read_csv("../train.csv").fillna("")
+## Pre-Processing of the Data ##
 
-    y = train.median_relevance.values    
-    
-    ## PORTER STEMMING ##
-    stemmer = PorterStemmer()
-    
+idTestx = test.id.values.astype(int)# isolating the IDs of the Test Data
+
+# drop ID, median_relevance and relevance_variance
+test = test.drop('id', axis=1)
+train = train.drop(['id','median_relevance', 'relevance_variance'], axis=1)
+
+
+## PORTER STEMMING ##
+
+# Create DataFrames for stemmed data
+stemmed_train = DataFrame(index=range(len(train["query"])),columns = list(train.columns.values))
+stemmed_test  = DataFrame(index=range(len(train["query"])),columns = list(test.columns.values))
+stemmer = PorterStemmer()
+
+for j in list(train.columns.values):
     for i in range(len(y)):
-        s=(" ").join(["q"+ z for z in BeautifulSoup(train["query"][i]).get_text(" ").split(" ")]) + " " + (" ").join(["z"+ z for z in BeautifulSoup(train.product_title[i]).get_text(" ").split(" ")]) + " " + BeautifulSoup(train.product_description[i]).get_text(" ")
+        s=(" ").join([z for z in BeautifulSoup(train[j][i]).get_text(" ").split(" ")])
         s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
-        traindata.append(s)
+        stemmed_train[j][i] = s
+    for i in range(len(idTestx)):
+        s=(" ").join([z for z in BeautifulSoup(test[j][i]).get_text(" ").split(" ")])
+        s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
+        stemmed_test[j][i] = s
 
-    # Initialize tf-idf vectorization function
+# do some lambda magic on text columns
 
-    tfv = TfidfVectorizer(min_df=3,  max_features=None,strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
+#traindata = list(stemmed_train.apply(lambda x:'%s %s %s' % (x['query'],x['product_title'], x['product_description']),axis=1))
+#testdata = list(stemmed_test.apply(lambda x:'%s %s %s' % (x['query'],x['product_title'], x['product_description']),axis=1))
+traindata = list(stemmed_train.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
+testdata = list(stemmed_test.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
+
+
+
+# Initialize tf-idf vectorization function
+
+tfv = TfidfVectorizer(min_df=3,  max_features=None,strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
  
-    tfv.fit(traindata)
-    X = tfv.transform(traindata)
+tfv.fit(traindata)
+X = tfv.transform(traindata)
 
-    # Initialize Model Variables #
+# Initialize Model Variables #
 
-    tSVD = TruncatedSVD(n_components=400)
-    scl  = StandardScaler()
-    svm  = SVC(C=10) 
+tSVD = TruncatedSVD(n_components=400)
+scl  = StandardScaler()
+svm  = SVC(C=10) 
 
-    #create sklearn pipeline
+#create sklearn pipeline
 
-    clf = pipeline.Pipeline([('tSVD', tSVD),('scl', scl),('svm', svm)])
-    
-    stemPred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
-    
-    print "Kappa Score for Training Data\nStemming\nScore=%f" %(quadratic_weighted_kappa(y, stemPred))
+clf = pipeline.Pipeline([('tSVD', tSVD),('scl', scl),('svm', svm)])
+
+stemPred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
+
+print "Kappa Score for Training Data\nStemming\nScore=%f" %(quadratic_weighted_kappa(y, stemPred))
