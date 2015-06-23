1694dee400f9542bbc8105eb37ab3197a849b45a
Anirudh <jonnalagadda.anirudh@gmail.com>
2015-06-23 16:12:29 +0530

    modified myPorterStemmer.py to incorporate use of StemmedCorpus function.

--- myPorterStemmer.py
+++ myPorterStemmer.py
@@ -1,11 +1,11 @@
 from pandas import read_csv, DataFrame
+from createCorpus import Corpus
 from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 from sklearn import pipeline
-from nltk.stem.porter import PorterStemmer
-from bs4 import BeautifulSoup
 from ml_metrics import quadratic_weighted_kappa
 from sklearn.cross_validation import cross_val_predict
 
@@ -15,6 +15,9 @@ test  = read_csv("../test.csv").fillna("")
 y = train.median_relevance.values
 
 ## Pre-Processing of the Data ##
+
+##In [4]: train = train.drop([list(train.columns.values)[i] for i in [0,4,5]],axis=1)
+
 
 idTestx = test.id.values.astype(int)# isolating the IDs of the Test Data
 
@@ -26,35 +29,27 @@ train = train.drop(['id','median_relevance', 'relevance_variance'], axis=1)
 ## PORTER STEMMING ##
 
 # Create DataFrames for stemmed data
-stemmed_train = DataFrame(index=range(len(train["query"])),columns = list(train.columns.values))
-stemmed_test  = DataFrame(index=range(len(train["query"])),columns = list(test.columns.values))
-stemmer = PorterStemmer()
-
-for j in list(train.columns.values):
-    for i in range(len(y)):
-        s=(" ").join([z for z in BeautifulSoup(train[j][i]).get_text(" ").split(" ")])
-        s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
-        stemmed_train[j][i] = s
-    for i in range(len(idTestx)):
-        s=(" ").join([z for z in BeautifulSoup(test[j][i]).get_text(" ").split(" ")])
-        s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
-        stemmed_test[j][i] = s
+stemmed_train = StemmedCorpus(train,index=range(len(train["query"])),columns = list(train.columns.values))
+stemmed_test  = StemmedCorpus(train,index=range(len(train["query"])),columns = list(test.columns.values))
 
 # do some lambda magic on text columns
 
-#traindata = list(stemmed_train.apply(lambda x:'%s %s %s' % (x['query'],x['product_title'], x['product_description']),axis=1))
-#testdata = list(stemmed_test.apply(lambda x:'%s %s %s' % (x['query'],x['product_title'], x['product_description']),axis=1))
-traindata = list(stemmed_train.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
-testdata = list(stemmed_test.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
-
+traindata1 = list(stemmed_train.apply(lambda x:'%s' % (x['query']),axis=1))
+testdata1 = list(stemmed_test.apply(lambda x:'%s' % (x['query']),axis=1))
+traindata2 = list(stemmed_train.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
+testdata2 = list(stemmed_test.apply(lambda x:'%s %s' % (x['query'],x['product_title']),axis=1))
 
 
 # Initialize tf-idf vectorization function
 
 tfv = TfidfVectorizer(min_df=3,  max_features=None,strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
  
-tfv.fit(traindata)
-X = tfv.transform(traindata)
+tfv.fit(traindata1)
+X1 = tfv.transform(traindata1)
+tfv.fit(traindata2)
+X2 = tfv.transform(traindata2)
+
+X = hstack((X1,X2))
 
 # Initialize Model Variables #
 
