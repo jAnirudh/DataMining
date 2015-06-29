026d3fd1093ade8603c669c727bce711252887c7
Anirudh <jonnalagadda.anirudh@gmail.com>
2015-06-29 14:58:10 +0530

    modified ensemble_stemming to incorporate extra features

--- ensemble_stemming.py
+++ ensemble_stemming.py
@@ -7,7 +7,7 @@ from scipy import hstack
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler
-from sklearn import pipeline, grid_search, metrics
+from sklearn import pipeline
 from ml_metrics import quadratic_weighted_kappa
 from sklearn.cross_validation import cross_val_predict
 from math import floor
@@ -20,13 +20,38 @@ testdata1  = []
 testdata2  = []
 
 #load data
-train = read_csv("train.csv").fillna("")
-test  = read_csv("test.csv").fillna("")
+train = read_csv("../train.csv").fillna("")
+test  = read_csv("../test.csv").fillna("")
 y = train.median_relevance.values
+idTestx = test.id.values.astype(int) # isolating the IDs of the Test Data
 
-## Pre-Processing of the Data ##
+import numpy
+from Levenshtein import jaro_winkler
 
-idTestx = test.id.values.astype(int)# isolating the IDs of the Test Data
+def score_title(query,title):
+    score = 0
+    for term in query.lower().split(" "):
+        if term in title.lower(): score += 1
+    return score
+
+def get_distance(source,destination,distance = jaro_winkler):
+    if type(source) == str : return distance(source,destination)
+    else: return distance(str(source),str(destination))
+
+trainFeatures = numpy.zeros((len(y),4))
+testFeatures  = numpy.zeros((len(idTestx),4))
+
+for i in range(len(y)):
+    trainFeatures[i,0] = get_distance(train["query"][i],train["product_title"][i])
+    trainFeatures[i,1] = get_distance(train["query"][i],train["product_description"][i])
+    trainFeatures[i,2] = len(train["query"][i].split(" "))
+    trainFeatures[i,3] = score_title(train["query"][i],train["product_title"][i])
+
+for i in range(len(idTestx)):
+    testFeatures[i,0] = get_distance(test["query"][i],test["product_title"][i])
+    testFeatures[i,1] = get_distance(test["query"][i],test["product_description"][i])
+    testFeatures[i,2] = len(test["query"][i].split(" "))
+    testFeatures[i,3] = score_title(test["query"][i],test["product_title"][i])
 
 # drop ID, median_relevance and relevance_variance
 test = test.drop('id', axis=1)
@@ -82,17 +107,13 @@ svd.fit(X2)
 X2 = svd.transform(X2)
 #X2_test = svd.transform(X2_test)
 
-# Reading data given by Bhavesh
-addFeature_train = read_csv("anirudh.csv")
-addFeature_test  = read_csv("anirudhTest.csv")
-
 # Initialize Model Variables #
 
 clf = pipeline.Pipeline([('scl', StandardScaler()),('svm', SVC(C=10,gamma=0.0002))])
 
 #Horizontally stacking the two matrices
-X = hstack((X1,X2,addFeature_train.as_matrix()))
-#X_test = hstack((X1_test,X2_test,addFeature_test.as_matrix()))
+X = hstack((X1,X2,trainFeatures))
+#X_test = hstack((X1_test,X2_test,testFeatures))
 
 stemPred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
 
@@ -127,8 +148,8 @@ X2 = svd.transform(X2)
 #X2_test = svd.transform(X2_test)
 
 #Horizontally stacking the two matrices
-X = hstack((X1,X2,addFeature_train.as_matrix()))
-#X_test = hstack((X1_test,X2_test,addFeature_test.as_matrix()))
+X = hstack((X1,X2,trainFeatures))
+#X_test = hstack((X1_test,X2_test,testFeatures))
 
 Pred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
 print "Kappa Score for Training Data\nWithout Stemming\nScore=%f" %(quadratic_weighted_kappa(y, Pred))
