3b792d0312368915202e67a3c11901eac4fd0f0b
Anirudh <jonnalagadda.anirudh@gmail.com>
2015-06-30 15:45:00 +0530

    incorporate sparsity reduction

--- stemming_req_SpRed.py
+++ stemming_req_SpRed.py
@@ -15,19 +15,40 @@ from sklearn.cross_validation import cross_val_predict
 
 traindata1 = []
 traindata2 = []
-testdata1  = []
-testdata2  = []
-train_rel  = []
-
 
 #load data
-train = read_csv("train.csv").fillna("")
-test  = read_csv("test.csv").fillna("")
+train = read_csv("../train.csv").fillna("")
+test  = read_csv("../test.csv").fillna("")
 y = train.median_relevance.values
+idTestx = test.id.values.astype(int) # isolating the IDs of the Test Data
 
-## Pre-Processing of the Data ##
+import numpy
+from Levenshtein import jaro_winkler
 
-idTestx = test.id.values.astype(int)# isolating the IDs of the Test Data
+def score_title(query,title):
+    score = 0
+    for term in query.lower().split(" "):
+        if term in title.lower(): score += 1
+    return score
+
+def get_distance(source,destination,distance = jaro_winkler):
+    if type(source) == str : return distance(source,destination)
+    else: return distance(str(source),str(destination))
+
+trainFeatures = numpy.zeros((len(y),4))
+testFeatures  = numpy.zeros((len(idTestx),4))
+
+for i in range(len(y)):
+    trainFeatures[i,0] = get_distance(train["query"][i],train["product_title"][i])
+    trainFeatures[i,1] = get_distance(train["query"][i],train["product_description"][i])
+    trainFeatures[i,2] = len(train["query"][i].split(" "))
+    trainFeatures[i,3] = score_title(train["query"][i],train["product_title"][i])
+
+for i in range(len(idTestx)):
+    testFeatures[i,0] = get_distance(test["query"][i],test["product_title"][i])
+    testFeatures[i,1] = get_distance(test["query"][i],test["product_description"][i])
+    testFeatures[i,2] = len(test["query"][i].split(" "))
+    testFeatures[i,3] = score_title(test["query"][i],test["product_title"][i])
 
 # drop ID, median_relevance and relevance_variance
 test = test.drop('id', axis=1)
@@ -45,9 +66,9 @@ for i in range(len(y)):
     s=(" ").join(["q"+ z for z in BeautifulSoup(train["query"][i]).get_text(" ").split(" ")]) + " " + (" ").join(["z"+ z for z in BeautifulSoup(train.product_title[i]).get_text(" ").split(" ")])
     s=re.sub("[^a-zA-Z0-9]"," ", s)
     s= (" ").join([stemmer.stem(z) for z in s.split(" ")])
-    train_rel.append(s)
     traindata2.append(s)
     traindata1.append(p)
+
 for i in range(len(idTestx)):
     p=(" ").join(["q"+ z for z in BeautifulSoup(test["query"][i]).get_text(" ").split(" ")])
     p=re.sub("[^a-zA-Z0-9]"," ", p)
@@ -60,12 +81,7 @@ for i in range(len(idTestx)):
 
 # Initialize tf-idf vectorization function
 
-#clf = pipeline.Pipeline([('v',TfidfVectorizer(min_df=5, max_df=500, max_features=None, strip_accents='unicode', analyzer='word', token_pattern=r'\w{1,}', ngram_range=(1, 2), use_idf=True, smooth_idf=True, sublinear_tf=True, stop_words = 'english')),
-#    ('svd', TruncatedSVD(n_components=200)),
-#    ('scl', StandardScaler()),
-#    ('svm', SVC(C=10.0))])
-
-tfv = TfidfVectorizer(min_df=1,max_features=None,strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
+tfv = TfidfVectorizer(min_df=1,max_features=None,strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1,2), use_idf=1,smooth_idf=1,sublinear_tf=1,stop_words = 'english')
 
 # Creating a Sparse Matrix for only query terms 
 tfv.fit(traindata1)
@@ -75,40 +91,35 @@ X1 = tfv.transform(traindata1)
 tfv.fit(traindata2)
 X2 = tfv.transform(traindata2)
 
+# Incorporate Sparse Reduction for product_title
+X2 = X2[:,numpy.squeeze(numpy.array(X2.sum(axis=0) > 3.5))]
+
+print X2.shape
+# Splitting Corpus into train and test again
+
+X1Test  = X1[len(y)+1:]
+X1      = X1[:len(y)]
+X2Test  = X2[len(y)+1:]
+X2      = X2[:len(y)]
+
 # Passing the vectorized matrices to SVD
 svd = TruncatedSVD(n_components = 800)
 svd.fit(X1)
 X1 = svd.transform(X1)
-svd = TruncatedSVD(n_components = 600)
+#X1Test  = svd.transform(X1Test)
+svd = TruncatedSVD(n_components = 1200)
 svd.fit(X2)
 X2 = svd.transform(X2)
+#X2Test = svd.transform(X2Test)
 
 # Initialize Model Variables #
 
-clf = pipeline.Pipeline([('scl', StandardScaler()),('svm', SVC(C=10.0))])
+clf = pipeline.Pipeline([('scl', StandardScaler()),('svm', SVC(C=10,gamma=0.0002))])
 
-#tSVD = TruncatedSVD(n_components=400)
-#scl  = StandardScaler()
-
-# pipeline for operating on X2
-#transform2feature = pipeline.Pipeline([('tSVD', tSVD),('scl', scl)])
-
-# transforming the sparse matrix of query and product_title to incorporate SVD and Scaling
-#transform2feature.fit(X2)
-#transform2feature.transform(X2)
-
-#Horizontally stacking the two sparse matrices
-X = hstack((X1[:len(y)],X2[:len(y)]))
-
-#Initialize SVM model
-#svm  = SVC(C=10) 
-
-#create sklearn pipeline
-
-#clf = pipeline.Pipeline([('scl', scl),('svm', svm)])
-
-# Classify the sparse stacked sparse matrix using two fold cross validation and model specified in line 80
-#stemPred = cross_val_predict(svm,X,y,cv=2,n_jobs=-1)
+#Horizontally stacking the two matrices
+X = hstack((X1,X2,trainFeatures))
+#X_test = hstack((X1Test,X2Test,testFeatures))
 
 stemPred = cross_val_predict(clf,X,y,cv=2,n_jobs=-1)
+
 print "Kappa Score for Training Data\nStemming\nScore=%f" %(quadratic_weighted_kappa(y, stemPred))
